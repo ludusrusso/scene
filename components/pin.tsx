dc9d3a97e71f9532901a0ccefe6a03eddd5ce3e3
Ludovico Russo <ludus.russo@gmail.com>
2022-02-13 11:26:55 +0100

    Add oldg integration thanks to Jaga and improve gui
    Co-authored-by: <jagasantagostino@gmail.com>

--- components/pin.tsx
+++ components/pin.tsx
@@ -36,29 +36,34 @@ const PinnedMessage = () => {
       }
     },
   });
+
   if (!message) {
     return null;
   }
   return (
-    <PinnedMessageStyled>
-      <h2 className="font-bold text-white text-2xl">{message.msg}</h2>
-      <p>{message.author}</p>
+    <PinnedMessageStyled className="">
+      <div className="w-full flex justify-center">
+        <div className="rounded-lg bg-stone-200 p-10 min-w-[700px] max-w-7xl gradient-border shadow-2xl ease-in duration-300">
+          <img
+            src="/gopher-dance.gif"
+            alt="dancing-gopher"
+            className="block mb-6"
+          />
+          <h2 className="font-bold text-stone-800 text-4xl">{message.msg}</h2>
+          <p>by {message.author}</p>
+        </div>
+      </div>
     </PinnedMessageStyled>
   );
 };
 
 const PinnedMessageStyled = styled.footer`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-
-  background: red;
-  padding: 10px;
+  position: fixed;
+  bottom: 250px;
+  width: 100%;
 `;
 
-const client = newApolloClientWithWs("http://localhost:8080/query");
+const client = newApolloClientWithWs("https://olg.dev.ludusrusso.space/query");
 export default function PinMessage() {
   return (
     <ApolloProvider client={client}>
